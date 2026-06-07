821db7e8bb6da1ec1bf9004939afc81fb30cade7
michiel1106 <michielimmink@gmail.com>
2026-06-07 19:09:15 +0200

    Initial commit

---
+++ src/client/java/bikerboys/clearviews/client/ClearviewsClient.java
@@ -0,0 +1,10 @@
+package bikerboys.clearviews.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ClearviewsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
